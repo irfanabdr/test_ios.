2d622ef1b4e5bf62a4be28a57e596333bffc6aef
Irfan <irfan.abdrasyid@gmail.com>
2024-11-20 16:10:23 +0700

    Initial commit

---
+++ JustLogin Test/Views/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MainViewController.swift
+//  JustLogin Test
+//
+//  Created by Irfan on 20/11/24.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
